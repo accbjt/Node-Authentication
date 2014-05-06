7d8daf0a29dc635e20a0a8acf587f5974e9a0047
Bill Tran <bjtran1234@gmail.com>
2014-05-05 19:18:08 -0700

    local login

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://bill:nodeauth@dbh56.mongolab.com:27567/nodeauth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
